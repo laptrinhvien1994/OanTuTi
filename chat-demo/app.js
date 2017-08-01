bf07ea378904b3c0914f066d4f3404ce0b8d43e2
laptrinhvien1994 <quang.nguyen.7878@gmail.com>
2017-08-01 23:07:06 +0700

    Update fmapp

--- chat-demo/app.js
+++ chat-demo/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./router');
+const https = require('https');
 const bodyParser = require('body-parser');
 const app = express();
 //Create an instance of Server and set up express app.
@@ -12,50 +13,44 @@ app.use(bodyParser.json());
 app.use('/resources', express.static('resources'));
 //route for API.
 app.use('/api',router);
-app.set('view engine', 'ejs');
 
+//Khi có kết nối từ Server làm server node kia  hoặc từ client  gửi lên
+//kết nối client vào room.
 
+var sendRequest = function(hostname, url, method, callback){
+  var options = {
+    hostname: hostname,
+    method: method,
+    path: url
+  }
+  var req = https.request(options, callback);
+  req.on('error', function(e){
+    console.log(e);
+  });
+}
 
-
-//Routing
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-app.get('*', function(req,res){
-  res.redirect('/');
-});
-
-app.get('/')
-
-//Socket Connection
-
-var sockets = [];
-
+//Lắng nghe sự kiện connection từ các clients.
 io.on('connection', function(socket){
-  //console.log('Client is connecting');
-  // socket.userID = socket.handshake.query.userID;
-  // console.log(socket.userID);
-  // sockets.push(socket.userID);
-  socket.on('client-message-content', function(data){
-    io.sockets.emit('server-message-content', data);
-    socket.broadcast.emit('server-typing-stopped', { senderID: data.senderID });
-  })
-  .on('client-is-typing', function(data){
-    socket.broadcast.emit('server-is-typing', data);
-  })
-  .on('client-typing-stopped', function(data){
-    socket.broadcast.emit('server-typing-stopped', data);
-  })
-  .on('client-send-choice', function(){
+  var pageID = socket.handshake.query.pageID;
+  socket.join(pageID);
+  //Gửi tín hiệu xuống server node kia để server node bên kia gửi thread conversation lên server.
+  socket.emit('sv-client-is-connected');
 
-  })
-  .on('client-send-start-signal', function(){
-
-  })
-
+  //Lắng nghe tín hiệu từ phía các clients.
+  socket.on('clt-send-conversation', function(data){
+    //Khi nhận được thread conversation
+    var threadID = data.threadId;
+    var queryStr = threadID + '...';
+    //Gọi API của Facebook để lấy nội dung của conversation.
+    sendRequest('https://facebook.com', queryStr, 'GET', function(res){
+      //Nhận xong thì gửi xuống cho user.
+      res.on('data', function(chunk){
+        io.to(pageID).emit('sv-send-conversation', chunk);
+      });
+    });
+  });
 });
 
 server.listen(9999, function(){
-  console.log('Server was started');
+  console.log('Server is listening on PORT 9999');
 });
--- chat-demo/client.js
+++ chat-demo/client.js
@@ -1,22 +1,34 @@
-
-
+$(document).ready(function(){
+  var token = 'EAACEdEose0cBAIoYFTbrvSnMCDsAkQFbC1FDK7fLjZCCQOzFZBHzf67isTbjZAMzvcHLeNbqeIEStXk34bpIkrtl86n6mGoZCt6UMvjn4tL9bJYpiQbZCHD0VpuSzKOCgAvWnoWgomQnggW0sHZBmmohONj4errhegR2wC9QwUhtdbRRo8xqWX7tcXJyuSYU55crEDPfhxzwZDZD';
+  var url = 'https://graph.facebook.com/v2.10/me/messages?access_token=' + token;
+  var setting = {
+    contentType: 'application/json; charset=utf-8',
+    method: 'POST',
+    data: JSON.stringify({
+      'recipient': {
+        'id': '383436965128019'
+      },
+      'message': {
+        'text': 'Chat demo via Facebook Graph API'
+      }
+    }),
+    success: function(data){
+      console.log(data);
+    },
+    error: function(e){
+      console.log(e);
+    }
+  };
+  $.ajax(url, setting);
+});
 
 //Page ID dùng để xác định Room.
 var pageID = null;
-var socket = io.connect('http://localhost:9999',{ query : "pageID="+ pageID });
+//var socket = io.connect('http://localhost:9999',{ query : "pageID="+ pageID });
 
+var accessToken = null;
 
-
-
-
-socket.on('sv-send-msg', function(data){
-
-});
-
-
-//Sau khi gửi data lên facebook API thì thực hiện emit dữ liệu lên server.
-socket.emit('clt-send-msg', data);
-
+//socket.on('sv-send-msg', function(data){});
 
 
 //Dùng để gửi tin nhắn.
@@ -29,20 +41,17 @@ var sendMsg = function(recipientID, msgContent){
     id : recipientID
   };
 
-  var payloadObj = new Object();
+  var payloadObj = {
+    text : msgContent
+  }
 
-  var detectResult = detectAttachmentService(msgContent);
-  //Nếu là gửi text
-  if(!detectResult){
-    payloadObj.text = msgContent;
-  }
-  //Nếu là gửi attachment
-  else {
-    payloadObj.attachment = msgContent;
-  }
   //Gọi Facebook API để gửi tin nhắn.
+  //https://developers.facebook.com/docs/graph-api/reference/v2.10/conversation/messages
   FB.api('')
 }
+
+//Sau khi gửi data lên facebook API thì thực hiện emit dữ liệu lên server.
+//socket.emit('clt-send-msg', data);
 
 var signalEnum = {
   typingOn: 0,
@@ -54,6 +63,7 @@ var msgContentType = {
   text: 1,
   attachment: 2
 }
+
 
 //Xác định định dạng của chuỗi tin nhắn cần gửi.
 //text : chuỗi cần xác định định dạng.
@@ -70,11 +80,4 @@ var detectHyperLink = function(text){
   var result = text.match(pattern);
   if(result == null) return false;
   return true;
-}
-
-
-
-
-var sendSignal = function(){
-
 }
